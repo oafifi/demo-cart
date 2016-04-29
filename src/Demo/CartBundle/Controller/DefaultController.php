56d881eb506f7772c53f6b5a54856126aea4d89c
Omar Afifi <omarafifi8@gmail.com>
2016-04-30 01:28:31 +0200

    create home page to test the project

--- src/Demo/CartBundle/Controller/DefaultController.php
+++ src/Demo/CartBundle/Controller/DefaultController.php
@@ -2,12 +2,24 @@
 
 namespace Demo\CartBundle\Controller;
 
+use Demo\CartBundle\Entity\Product\ShoppingItem;
+use Doctrine\Common\Collections\ArrayCollection;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
-    public function indexAction()
+    public function indexAction(Request $request)
     {
+
+        //Setup for testing purpose, create cart if not found and deal as if it is the user cart
+        $cm = $this->get("demo_cart.cart_manager");
+        if(!($cm->getUserCart())){
+            $cm->create();
+        }
+
         return $this->render('DemoCartBundle:Default:index.html.twig');
     }
 }
