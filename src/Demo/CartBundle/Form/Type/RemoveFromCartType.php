4df7e580d1b589e96fb4544718920e026d591a32
Omar Afifi <omarafifi8@gmail.com>
2016-04-30 01:26:01 +0200

    add cart control forms

---
+++ src/Demo/CartBundle/Form/Type/RemoveFromCartType.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Omar
+ * Date: 4/29/2016
+ * Time: 7:04 AM
+ */
+
+namespace Demo\CartBundle\Form\Type;
+
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class RemoveFromCartType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('order_item_id', HiddenType::class)
+            ->add('remove_from_cart', SubmitType::class, array('label' => 'Remove'))
+        ;
+    }
+}
